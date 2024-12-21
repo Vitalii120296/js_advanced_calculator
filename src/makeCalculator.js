4e2fd20289d74b847e377b108385c8e238615cf7
Vitalii <v.hulaievych@gmail.com>
2024-12-21 19:53:31 +0000

    failed

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -3,8 +3,48 @@
 /**
  * @return {object}
  */
-function makeCalculator() {
-  // write code here
+function makeCalculator(result = 0) {
+  const calculate = {
+    result,
+
+    operate(action, value = 0) {
+      this.result = action(value);
+
+      return this.result;
+    },
+
+    add(value = 0) {
+      this.result += value;
+
+      return this;
+    },
+
+    subtract(value = 0) {
+      this.result -= value;
+
+      return this;
+    },
+
+    multiply(value = 0) {
+      this.result *= value;
+
+      return this;
+    },
+
+    divide(value = 0) {
+      this.result /= value;
+
+      return this;
+    },
+
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculate;
 }
 
 module.exports = makeCalculator;
